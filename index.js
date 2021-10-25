c9dcb240db46c860917bfe364e205fe73de7d585
Connie Tran Hedberg <connietranhedberg@gmail.com>
2021-10-25 13:41:50 +0200

    Fix JavaScript and clean up syntax

---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * The main JavaScript file of the application.
+ *
+ * @author Connie Tran Hedberg <connietranhedberg@gmail.com>
+ * @version 1.0.0
+ */
+
+
+/** 
+ * Click event - drops down and fold a menu when user clicks on the button.
+ * 
+ */ 
+const button = document.getElementById("btn")
+const list = document.getElementById("list")
+list.style.display = "none"
+button.addEventListener("click", (event) => {
+    if (list.style.display == "none") {
+        list.style.display = "block"
+    } 
+    else {
+        list.style.display = "none"
+    } 
+})
+
+/** 
+ * Click event - Change color on heart when clicked.
+ * 
+ */
+const hearts = document.querySelectorAll(".heart")
+for (const heart of hearts) {
+    heart.addEventListener('click', () => {
+        heart.classList.toggle('red')
+    })
+}
+
+/** 
+ * Click event - Change color on star when clicked.
+ * 
+ */
+const stars = document.querySelectorAll(".star")
+for (const star of stars) {
+    star.addEventListener("click", () => {
+        star.classList.toggle('orange')
+    })
+}
+
+
